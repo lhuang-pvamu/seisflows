5254b1f5941e59ef4190fbaf8207316b6868df22
Jeremy Kemp <jakemp@uh.edu>
2018-09-20 17:27:11 -0400

    moving new paths.py into marmousi 500

--- examples/marmousi-waterdepth-500/paths.py
+++ examples/marmousi-waterdepth-500/paths.py
@@ -1,13 +1,13 @@
 
 DATA = ''
 
-MODEL_INIT = '/home/lhuang/mySeisflows/examples/marmousi-waterdepth-500/smooth_0150'
+MODEL_INIT = './smooth_0150'
 
-MODEL_TRUE = '/home/lhuang/mySeisflows/examples/marmousi-waterdepth-500/model_true'
+MODEL_TRUE = './model_true'
 
 PRECOND = ''
 
-SPECFEM_DATA = '/home/lhuang/mySeisflows/examples/marmousi-waterdepth-500/specfem2d/DATA'
+SPECFEM_DATA = './specfem2d/DATA'
 
-SPECFEM_BIN = '/home/lhuang/specfem/specfem2d/bin'
+SPECFEM_BIN = './specfem2d/bin'
 
