83ddecbd622088281afe58f927c4cfb2e6902cd6
Lei Huang <lhuang@pvamu.edu>
2018-09-19 23:49:41 -0400

    LH: remove some binaries, revise the path and Par_file.

--- examples/marmousi-waterdepth-0/paths.py
+++ examples/marmousi-waterdepth-0/paths.py
@@ -1,13 +1,13 @@
 
 DATA = ''
 
-MODEL_INIT = '/home/jakemp/code/examples/marmousi-waterdepth-0/smooth_0150'
+MODEL_INIT = '/home/lhuang/mySeisflows/examples/marmousi-waterdepth-0/smooth_0150'
 
-MODEL_TRUE = '/home/jakemp/code/examples/marmousi-waterdepth-0/model_true'
+MODEL_TRUE = '/home/lhuang/mySeisflows/examples/marmousi-waterdepth-0/model_true'
 
 PRECOND = ''
 
-SPECFEM_DATA = '/home/jakemp/code/examples/marmousi-waterdepth-0/specfem2d/DATA'
+SPECFEM_DATA = '/home/lhuang/mySeisflows/examples/marmousi-waterdepth-0/specfem2d/DATA'
 
-SPECFEM_BIN = '/home/jakemp/code/examples/marmousi-waterdepth-0/specfem2d/bin'
+SPECFEM_BIN = '/home/lhuang/specfem/specfem2d/bin'
 
