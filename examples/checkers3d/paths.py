409526cc6882cc16e81f0b275b1392003cb227ca
Jeremy Kemp <jakemp@uh.edu>
2018-10-22 15:25:29 -0400

    adding checkers3d

---
+++ examples/checkers3d/paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = './model_init_gll'
+
+MODEL_TRUE = './model_true_gll'
+
+PRECOND = ''
+
+SPECFEM_DATA = './specfem3d-b5559822/DATA'
+
+SPECFEM_BIN = '../../../specfem3d/bin'
+
