8e4b6b3ce38f5c20be0be8839c6e1bed57493308
Ted Clee <T_Clee@acm.org>
2019-07-10 12:56:57 -0500

    RedSea setup files

---
+++ examples/RedSea-AC/paths.py
@@ -0,0 +1,24 @@
+CASE = 'RedSea-AC-Noflip'
+
+SF_TOP = '/home/teclee/DAC/FWI/SpecFEM/seisflows/'
+
+SPECFEM_BIN = SF_TOP+'../specfem2d/bin'
+
+SPECFEM_DATA = './specfem2d/DATA'
+
+SF_SCRATCH = '/scratch/teclee/seisdata/SFtests/'
+
+#LOCAL = '/ssd1/teclee/'+CASE
+
+WORKDIR = SF_SCRATCH+CASE
+
+DATA = ''
+
+MODEL_TRUE = './model_true_pml'
+
+MODEL_INIT = './model_init_smooth'
+
+MASK = './mask'
+
+PRECOND = ''
+
